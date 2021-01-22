d2eca1b86b2ffe76465ced1eb22934304a1bf550
Alex de Souza <alex94tu@gmail.com>
2021-01-22 10:59:50 -0300

    Testando app no despositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
-    bucketBaseUrl: "https://curso-spring-boot-2021.s3-sa-east-1.amazonaws.com"
+    baseUrl: "https://curso-spring-2021.herokuapp.com",
+    bucketBaseUrl: "https://curso-spring-boot-2021.s3-sa-east-1.amazonaws.com",
+    baseUrlLocalHost: "http://localhost:8080"
 }
